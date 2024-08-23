124aeced361c7f3526c49c2fcfe3269280c91f6c
Spencer, Jonathan F - St Louis, MO <Jonathan.F.Spencer@usps.gov>
2024-08-23 09:20:27 -0500

    test adding a pet

--- src/test/java/ip/swagger/petstore/PetStoreTest.java
+++ src/test/java/ip/swagger/petstore/PetStoreTest.java
@@ -1,7 +1,12 @@
 package ip.swagger.petstore;
 
 import io.restassured.RestAssured.*;
+import io.restassured.http.ContentType;
 import io.restassured.matcher.RestAssuredMatchers.*;
+import io.restassured.response.Response;
+  
+import static io.restassured.RestAssured.given;  
+import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.BeforeClass;
 import org.junit.AfterClass;
@@ -48,4 +53,40 @@ public class PetStoreTest {
     // Assert that the response is valid
     assertEquals(200, response.getStatusLine().getStatusCode());
   }
+
+  @Test  
+  public void testAddPet() {  
+      // Create a new pet object  
+      String newPet = "{\n" +  
+              "  \"id\": 123456,\n" +  
+              "  \"name\": \"TestPet\",\n" +  
+              "  \"photoUrls\": [\n" +  
+              "    \"http://example.com/photo1\"\n" +  
+              "  ],\n" +  
+              "  \"tags\": [\n" +  
+              "    {\n" +  
+              "      \"id\": 1,\n" +  
+              "      \"name\": \"tag1\"\n" +  
+              "    }\n" +  
+              "  ],\n" +  
+              "  \"status\": \"available\"\n" +  
+              "}";  
+
+      // Send POST request to add the new pet  
+      Response response = given()  
+              .contentType(ContentType.JSON)  
+              .body(newPet)  
+              .when()  
+              .post("http://localhost:8080/api/v3/pet")  
+              .then()  
+              .statusCode(200)  
+              .body("id", equalTo(123456))  
+              .body("name", equalTo("TestPet"))  
+              .body("status", equalTo("available"))  
+              .extract()  
+              .response();  
+        
+      System.out.println("Response: " + response.asString());  
+  } 
+  
 }
